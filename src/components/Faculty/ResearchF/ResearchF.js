b3dacf54e4fcc547fba2e21eeb96cb53744b1e88
Candyman770 <isaac2311.cl@gmail.com>
2020-07-22 21:55:33 +0530

    Research (Faculty)

--- src/components/Faculty/ResearchF/ResearchF.js
+++ src/components/Faculty/ResearchF/ResearchF.js
@@ -2,35 +2,25 @@
 // import "../node_modules/bootstrap/dist/css/bootstrap.css";
 // then add this path in app.js to use bootstrap
 
-import React, { Component } from 'react'
-import AddResearch from './AddResearchF'
+import React from 'react';
 
+import classes from './ResearchF.module.css';
 
-export default class RaiseReq extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-             addResearchPaper : false
-        }
-    }
-    render() {
+const Research=props=>{
+    return (
+        <div className={classes.Research}>
+        	<div>
+        		<h1>No research is ever quite complete. It is the glory of a good bit of work that it 
+        		opens the way for something still better, and this repeatedly leads to its own eclipse.</h1>
+        		<h2> - Mervin Gordon </h2>
+        		<form>
+        			<p>Upload Your Research Paper</p>
+        			<input type='file' /><br />
+        			<button>SUBMIT</button>
+        		</form>
+        	</div>
+        </div>
+    );
+};
 
-        let addResearchPaper=() => this.setState({addResearchPaper : false})
-
-        return (
-            <div>
-                <h1 style={{textAlign:"center"}}>Research Paper </h1>
-                <br/>
-                <br/>
-                <div style={{textAlign:"center"}}>
-                <button className="btn btn-primary my-3 mx-2 w-25 p-3" onClick={() =>
-                this.setState({addResearchPaper:true})}>Research Paper</button><br></br>
-                </div>
-                <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-                <AddResearch 
-                show={this.state.addResearchPaper}
-                onHide={addResearchPaper} />
-            </div>
-        )
-    }
-}
+export default Research;
