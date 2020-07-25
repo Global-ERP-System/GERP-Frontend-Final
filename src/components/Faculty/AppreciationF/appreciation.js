b8fc8a8967cb14eb91f924b722c2af5aeb32b93e
Candyman770 <isaac2311.cl@gmail.com>
2020-07-25 20:33:20 +0530

    AppreciationF (Styling Fix)

--- src/components/Faculty/AppreciationF/appreciation.js
+++ src/components/Faculty/AppreciationF/appreciation.js
@@ -7,19 +7,19 @@ import BadgeClass from './badge.js';
 class Appreciation extends React.Component{
     render(){
         return (
-      <div>
+      <div className='Appreciate'>
 	            
             <Card>
                <CardTitle><h2 className="text-center">APPRECIATION PAGE</h2></CardTitle>
                <br/><br/>
-                <CardBody style={{marginLeft:"5%",marginRight:"5%"}}>                	                	
+                <CardBody >                	                	
 
 	                <form className="form-group">  
 		                <label>Student Roll No :</label><br/>
-                        <input className="form-control" type="text" name="roll" style={{width:"50%"}} 
+                        <input className="form-control" type="text" name="roll" style={{width:"75%",margin:'0'}} 
                         placeholder="Enter student roll number" /><br/>
 		                <label>Appreciation Message: </label><br/>
-		                <textarea className="form-control" placeholder="Message" style={{width:"50%"}}></textarea><br/>	
+		                <textarea className="form-control" placeholder="Message" style={{width:"75%"}}></textarea><br/>	
                         <label>Give a Badge : </label><br/>
                             <BadgeClass/><br/> 
 
