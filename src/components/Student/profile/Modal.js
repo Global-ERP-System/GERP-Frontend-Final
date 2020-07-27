6de3245381f249ef2018cdc4183cfb978a062aa2
Riya Goel <riyagoel192@gmail.com>
2020-07-27 14:41:54 +0530

    chamges to student profile

--- src/components/Student/profile/Modal.js
+++ src/components/Student/profile/Modal.js
@@ -14,94 +14,75 @@ const Create = (props) => {
 
   return (
   <div>
-    <Button color="primary" onClick={toggle} className="button1">Edit Details</Button>
-    <Modal isOpen={modal} toggle={toggle} contentClassName="assignment-modal">
-      <ModalHeader style={{backgroundColor:'#f5f5f5'}} toggle={toggle} close={closeBtn}>Assignment</ModalHeader>
+    <Button color="primary" onClick={toggle} className="button1" style={{width:'130px',height:'40px'}}>Edit Details</Button>
+    <Modal isOpen={modal} toggle={toggle} contentClassName="assignment-modal"   style={{width:'600px'}}>
+      <ModalHeader style={{backgroundColor:'#f5f5f5'}} toggle={toggle} close={closeBtn}>Profile</ModalHeader>
       <ModalBody>
-      <div className="container" style={{paddingTop:'0',paddingBottom:'0',marginTop:'20px'}}>
-          
-          <Form className="form1"> 
-              <Row form>
-                  <Col md={4}>
-                  <FormGroup>
-                      <Label for="class">Class</Label>
-                      <CustomInput type="select" id="class">
-                          <option>Class</option>
-                          <option>CS8B</option>
-                          <option>EC8A</option>
-                          <option>CS6A</option>
-                          <option>CS8A</option>
-                      </CustomInput>
-                  </FormGroup>
-                  </Col>
-                  <Col md={4}>
-                  <FormGroup>
-                      <Label for="class">Subject</Label>
-                      <CustomInput type="select" id="class">
-                          <option>Subject</option>
-                          <option>Data Mining</option>
-                          <option>Operating System</option>
-                          <option>Embedded System</option>
-                          <option>Cryptography</option>
-                      </CustomInput>
-                  </FormGroup>
-                  </Col>
-                  <Col md={4}>
-                  <FormGroup>
-                      <Label for="student">For</Label>
-                      <CustomInput type="select" id="student">
-                          <option>All</option>
-                          <option>Select few </option>
-                      </CustomInput>
-                  </FormGroup>
-                  </Col>
-              </Row>
-              <Row form>
-                  <Col md={8}>
-                  <FormGroup>
-                  <Label for="topic">Topic</Label>
-                  <Input type="text" id="topic" />
-                  </FormGroup>
-                  </Col>
-                  <Col md={4}>
-                  <FormGroup>
-                  <Label for="marks">Marks</Label>
-                  <Input type="text" id="marks" />
-                  </FormGroup>
-                  </Col>
-              </Row>
-              <Row form>
-                  <Col md={12}>
-                  <FormGroup>
-                  <Label for="desc">Description</Label>
-                  <Input type="textarea" id="desc" placeholder="Instructions(optional)" />
-                  </FormGroup>
-                  </Col>
-              </Row>
-              <Row form>
-                  <Col md={6}>
-                  <FormGroup>
-                      <Label for="class">Due Date</Label>
-                      <Input type="date" id="date" />
-                  </FormGroup>
-                  </Col>
-                  <Col md={6}>
-                  <FormGroup>
-                      <Label for="class">Due Time</Label>
-                      <Input type="time" id="time" />
-                  </FormGroup>
-                  </Col>
-                  </Row>
-              <Row form>
-                  <Input type="file" id="file" />
-              </Row>
-          </Form>
-          
-      </div>
+      
+            <div style={{height:'360px',width:'228px',float:'left'}}>
+
+                <label>Registration No.</label>
+                <br></br>
+                <input className="form-control" style={{width:'100%',fontSize:'15px'}} placeholder="Enter registration number"/>
+                
+                <br></br>
+
+                <label>First Name</label>
+                <br></br>
+                <input className="form-control" style={{width:'100%',fontSize:'15px'}} placeholder="Enter first name"/>
+
+                <br></br>
+
+                <label>Parent Name</label>
+                <br></br>
+                <input className="form-control" style={{width:'100%',fontSize:'15px'}} placeholder="Enter parent name"/>
+
+                <br></br>
+
+                <label>Phone No.</label>
+                <br></br>
+                <input className="form-control" style={{width:'100%',fontSize:'15px'}} placeholder="Enter phone number"/>
+
+            </div>
+
+            <div style={{height:'360px',width:'228px',float:'left',marginLeft:'10px'}}>
+
+            <label>Roll No.</label>
+                <br></br>
+                <input className="form-control" style={{width:'100%',fontSize:'15px'}} placeholder="Enter registration number"/>
+                
+                <br></br>
+
+                <label>Last Name</label>
+                <br></br>
+                <input className="form-control" style={{width:'100%',fontSize:'15px'}} placeholder="Enter first name"/>
+
+                <br></br>
+
+                <label>Email</label>
+                <br></br>
+                <input className="form-control" style={{width:'100%',fontSize:'15px'}} placeholder="Enter parent name"/>
+
+                <br></br>
+
+                <label>Blood Group</label>
+                <br></br>
+                <input className="form-control" style={{width:'100%',fontSize:'15px'}} placeholder="Enter phone number"/>
+
+            </div>
+            {/* <label style={{fontSize:'14px',fontWeight:'bold'}}>Registration No.</label>
+            <br></br>
+            <input className="form-control" style={{width:'50%',fontSize:'14px'}} placeholder="Enter registration number"/>
+
+            <label style={{fontSize:'14px',fontWeight:'bold'}}>Roll No.</label>
+            <br></br>
+            <input className="form-control" style={{width:'50%',fontSize:'14px'}} placeholder="Enter roll number"/>         */}
+
+
       </ModalBody>
       <ModalFooter>
         <Col>
-        <Button type= "submit" color="primary" onClick={toggle}>Assign</Button>{' '}
+        <Button type= "submit" color="primary" onClick={toggle}>Submit</Button>{' '}
         </Col>
         <Col>
         <Button style={{marginLeft:'80px'}} color="secondary" onClick={toggle}>Cancel</Button>
