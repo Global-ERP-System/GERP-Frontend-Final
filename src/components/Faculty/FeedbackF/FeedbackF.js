b07c46ffc84e75d4fb8503c6feef66e1862c99a0
Candyman770 <isaac2311.cl@gmail.com>
2020-08-04 19:50:36 +0530

    Feedback/Intern/RaiseReq changed

--- src/components/Faculty/FeedbackF/FeedbackF.js
+++ src/components/Faculty/FeedbackF/FeedbackF.js
@@ -1,129 +1,49 @@
 import React from 'react';
-import {
-  Card
-} from 'reactstrap'
+//import {Card} from 'reactstrap'
 import StarRatingComponent from 'react-star-rating-component';
-import "./FeedbackF.css";
+import classes from "./FeedbackF.module.css";
 
-class Feedback extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      rating: 1
-    };
+const feedbacks=[
+  {
+    name:'Student - 1',
+    class:'Computer Science - 5A',
+    comment:'You continue to be excellent in your teaching, both in the classroom and online.You are a teacherwho devotes a great amount of personal effort and interest to your daily teaching activities, as well as to your students.As one would expect, student evaluations of your teaching remain very high in all categories.',
+    stars:5
+  },{
+    name:'Student - 2',
+    class:'Computer Science - 4B',
+    comment:'You continue to be excellent in your teaching, both in the classroom and online.You are a teacherwho devotes a great amount of personal effort and interest to your daily teaching activities, as well as to your students',
+    stars:4
+  },{
+    name:'Student - 3',
+    class:'Computer Science - 3C',
+    comment:'You continue to be excellent in your teaching, both in the classroom and online.You are a teacherwho devotes a great amount of personal effort and interest to your daily teaching activities, as well as to your students',
+    stars:5
+  },{
+    name:'Student - 4',
+    class:'Computer Science - 1A',
+    comment:'You continue to be excellent in your teaching, both in the classroom and online.You are a teacherwho devotes a great amount of personal effort and interest to your daily teaching activities, as well as to your students',
+    stars:4
   }
+]
 
-  render() {
-    const {
-      rating
-    } = this.state;
-    const mystyle = {
-      color: "orangered",
+const Feedback=props=>{
+  const feeds=feedbacks.map(feed=>{
+    return <li>
+        <h2>{feed.name}</h2>
+        <h3>{feed.class}</h3>
+        <StarRatingComponent className={classes.Stars} name = "rate1" editing = {false} starCount = {5} value = {feed.stars} />
+        <p>{feed.comment}</p>
+      </li>
+  })
+  return (
+    <div className={classes.Feedback}>
+      <h1>Feedback</h1>
+      <ul>
+        {feeds}
+      </ul>
+    </div>
+  );
+};
 
-      padding: "10px",
-      width: "100%",
-      fontFamily: "Arial",
-      textalign: "center",
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center"
-    }
-
-    return ( <div>
-      <h1 style = {
-        mystyle
-      } > < b > Feedback </b></h1 >
-      <Card className = "Card" >
-      <div className = "div" >
-      <h4 style = {
-        {
-          color: "blue"
-        }
-      }> Student - 1 </h4>   <h6 style = {
-        {
-          color: "gray"
-        }
-      } > computer science - 5 A </h6> <
-      StarRatingComponent name = "rate1"
-      editing = {
-        false
-      }
-      starCount = {
-        5
-      }
-      value = {
-        4
-      }
-      /> <h6> You
-      continue to be excellent in your teaching, both in the classroom and online.You are a teacher
-      who devotes a great amount of personal effort and interest to your daily teaching activities, as well as to your students.As one would expect, student evaluations of your teaching remain very high in all categories. </h6> </div > </Card >  <Card className = "Card" >
-      <div className = "div" >
-      <h4 style = {
-        {
-          color: "blue"
-        }
-      } > Student - 2 </h4>   <h6 style = {
-        {
-          color: "gray"
-        }
-      } > computer science - 5 A </h6> <
-      StarRatingComponent name = "rate1"
-      editing = {
-        false
-      }
-      starCount = {
-        5
-      }
-      value = {
-        5
-      }
-      /> <h6 > excellent </h6> </div > </Card >  <Card className = "Card" >
-      <div className = "div" >
-      <h4 style = {
-        {
-          color: "blue"
-        }
-      } > Student - 3 </h4>   <h6 style = {
-        {
-          color: "gray"
-        }
-      } > computer science - 5 A </h6> <
-      StarRatingComponent name = "rate1"
-      editing = {
-        false
-      }
-      starCount = {
-        5
-      }
-      value = {
-        4
-      }
-      /> <h6 > good </h6> </div > </Card >  <Card className = "Card" >
-      <div className = "div" >
-      <h4 style = {
-        {
-          color: "blue"
-        }
-      } > Student - 4 </h4>   <h6 style = {
-        {
-          color: "gray"
-        }
-      } > computer science - 4 A </h6> <
-      StarRatingComponent name = "rate1"
-      editing = {
-        false
-      }
-      starCount = {
-        5
-      }
-      value = {
-        3
-      }
-      /> <h6 > good </h6> </div > </Card > 
-
-      </div>
-    );
-  }
-}
 export default Feedback;
