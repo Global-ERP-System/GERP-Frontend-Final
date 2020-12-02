8fe067bb330787e4dfe79827f26e8525ce4fa355
tushar-bht <tusharbhtt67@gmail.com>
2020-12-03 01:06:50 +0530

    fixed sideDrawer

--- src/components/Header-Footer/SideDrawer/SideDrawer.js
+++ src/components/Header-Footer/SideDrawer/SideDrawer.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import './SideDrawer.css';
-import Intern from '../../Student/internship/intern'
-import {BrowserRouter as Router,Route,Link,Switch,Redirect} from 'react-router-dom'
-const sideDrawer = props => {
-    let drawerClasses = 'side-drawer';
-    if(props.show) {
-        drawerClasses = 'side-drawer open'
-    }
-    return (
-  
-    <nav className={drawerClasses} style={{height:'240px',width:'10%'}}>
-        <ul>
-            <li style={{marginTop:'-20px'}}><Link to="/raisereq" style={{marginLeft:'-32px'}}>Raise a Request</Link></li>
-            <li><Link to="/recommendation" style={{marginLeft:'-32px',float:'left'}}>Recommendation Badges</Link></li>
-            <li><Link to="/relation" style={{marginLeft:'-32px'}}>My Relations</Link></li>
-            <li><Link to="/intern" style={{marginLeft:'-32px'}}>Internships</Link></li>
-        </ul>
+import React from "react";
+import "./SideDrawer.css";
+import { Link } from "react-router-dom";
+
+const sideDrawer = (props) => {
+  return (
+    <nav className="side-drawer-nav">
+      <p>
+        <Link to="/raisereq">Raise a Request</Link>
+      </p>
+      <p>
+        <Link to="/recommendation">Recommendation Badges</Link>
+      </p>
+      <p>
+        <Link to="/relation">My Relations</Link>
+      </p>
+      <p>
+        <Link to="/intern">Internships</Link>
+      </p>
     </nav>
-   
-    );
+  );
 };
 
 export default sideDrawer;
