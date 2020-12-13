d1273d4ffd9c943b87f7d48169c711c99958799d
Anubhav Saha <anubhavvsaha2001@gmail.com>
2020-12-13 15:39:55 +0530

    Fixed Student Fees Page

--- src/components/Student/Fee Structure/FeeStructure.js
+++ src/components/Student/Fee Structure/FeeStructure.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import './FeeStructure.css'
-import { DropdownButton, ButtonGroup, Dropdown, Form, FormGroup, CustomInput, Label, Row, Col, Input, Button } from 'react-bootstrap';
+import { DropdownButton, ButtonGroup, Dropdown, Row, Col } from 'react-bootstrap';
 import {MDBFormInline} from 'mdbreact';
 
 export default class FeeStructure extends Component {
@@ -50,10 +50,10 @@ export default class FeeStructure extends Component {
 
     render() {
         return (
-            <div style={{ marginLeft: "20%", marginRight: "20%", height: "100%", paddingLeft: '20px', paddingRight: '20px'}}>
+            <div className='cont'>
                 <h1 className="Heading" style={{ textAlign: "center", fontFamily: 'Montserrat', fontWeight: '700'}}>Fee Structure</h1>
                 <hr style={{ width: '80%' }}></hr>
-                {/* <form onSubmit={this.handlesubmit} style={{ marginTop: '0px' }}>
+                <form onSubmit={this.handlesubmit} style={{ marginTop: '0px' }}>
                         <Row>
                             <Col sm={5}>
                                 <label style={{ fontFamily: 'Pt sans narrow ', fontWeight: '700' }}>Name:</label>
@@ -61,7 +61,6 @@ export default class FeeStructure extends Component {
                             <Col sm={7}>
                                 <input className="form-control" value={this.state.name} onChange={this.handlename} required placeholder="Name" style={{ fontFamily: 'Montserrat Regular', fontWeight: '400' }}/>
                             </Col>
-
                         </Row>
                         <br></br>
                         <Row>
@@ -74,26 +73,29 @@ export default class FeeStructure extends Component {
                             </Row>
                         <br></br>
                         <Row>
-                        
-
-                            <MDBFormInline style={{ marginTop: '0px', marginLeft:'50%' }}>
-                                    <select title="Semester" className="form-control" name="Semester" placeholder="Select semester" style={{ fontFamily: 'Montserrat Regular', fontWeight: '400' }}>
+                            <Col sm={5}>
+                                <label style={{ fontFamily: 'Pt sans narrow ', fontWeight: '700' }}>Semester:</label>
+                            </Col>
+                            <Col sm={7}>
+                                <select title="Semester" className="form-control" name="Semester" placeholder="Select semester" style={{ fontFamily: 'Montserrat Regular', fontWeight: '400' }}>
                                     <option >Select semester</option>
                                     <option eventKey="1">I</option>
                                     <option eventKey="2">II</option>
-                                </select>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                                <select title="Subject Details" className="form-control" name="Subject Details" placeholder="Select subject" style={{ fontFamily: 'Montserrat Regular', fontWeight: '400' }}>
+                                </select>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col sm={5}>
+                                <label style={{ fontFamily: 'Pt sans narrow ', fontWeight: '700' }}>Sunject:</label>
+                            </Col>
+                            <Col sm={7}>
+                            <select title="Subject Details" className="form-control" name="Subject Details" placeholder="Select subject" style={{ fontFamily: 'Montserrat Regular', fontWeight: '400' }}>
                                     <option eventKey="1">Subject details</option>
                                     <option eventKey="2">Subject details</option>
                                 </select>
-                                </MDBFormInline>
-                            
-                            
+                            </Col>
                         </Row>
-                            
                            <br></br>
-
-                        
                         <Row>
                             <Col sm={5}>
                                 <label style={{ fontFamily: 'Pt sans narrow ', fontWeight: '700' }}>Course:</label>
@@ -102,10 +104,8 @@ export default class FeeStructure extends Component {
                                 <input className="form-control" value={this.state.course}
                                     onChange={this.handlecourse} required placeholder="Course" style={{ fontFamily: 'Montserrat Regular', fontWeight: '400' }}/>
                             </Col>
-
                         </Row>
                         <br></br>
-
                         <div >                   
                             <Row>
                                 <Col sm={5}>
@@ -115,7 +115,6 @@ export default class FeeStructure extends Component {
                                     <input className="form-control" value={this.state.Previousfeestatus}
                                         onChange={this.handlePreviousfeestatus} required style={{ fontFamily: 'Montserrat Regular', fontWeight: '400' }}/>
                                 </Col>
-
                             </Row>
                             <br></br>
                             <Row>
@@ -126,7 +125,6 @@ export default class FeeStructure extends Component {
                                     <input className="form-control" value={this.state.duefeestatus}
                                         onChange={this.handleduefeestatus} required style={{ fontFamily: 'Montserrat Regular', fontWeight: '400' }}/>
                                 </Col>
-
                             </Row>
                             <br></br>
                             <Row>
@@ -137,10 +135,7 @@ export default class FeeStructure extends Component {
                                     <input className="form-control" value={this.state.duefeeamount}
                                         onChange={this.handleduefeeamount} required style={{ fontFamily: 'Montserrat Regular', fontWeight: '400' }}/>
                                 </Col>
-
                             </Row>
-
-                            
                             <br></br>
                             <Row>
                                 <Col sm={5}>
@@ -150,26 +145,19 @@ export default class FeeStructure extends Component {
                                     <input className="form-control" value={this.state.lastdateforpayment}
                                         onChange={this.handlelastdateforpayment} required style={{ fontFamily: 'Montserrat Regular', fontWeight: '400' }}/>
                                 </Col>
-
                             </Row>
-
-                           
                             <br></br>
                         </div>   
-                    
                         <div className="Display_Button">
-                            <DropdownButton as={ButtonGroup} title="Payment Mode" name="See Details" id="bg-vertical-dropdown-1" variant="primary" className="mx-3">
+                            <DropdownButton as={ButtonGroup} title="Payment Mode" name="See Details" variant="primary" className="mx-3">
                                 <Dropdown.Item eventKey="1">Subject Details</Dropdown.Item>
                                 <Dropdown.Item eventKey="2">Subject Details</Dropdown.Item>
                             </DropdownButton>
                             
-                        <button id="Pay" style={{ color: "white", backgroundColor: '#138808' }} type="submit" className="btn">Pay</button>
-                           
+                        <button id="Pay" type="submit" className="btn">Pay</button>
                         </div>
-                    </form> */}
-
+                    </form>
                     <br></br>
-
                 <div id="year1">
                     <div className="SearchPeople">&lt;Year&gt; </div>
                     <div className="SearchPeople">&lt;College_Name&gt;</div>
@@ -177,13 +165,9 @@ export default class FeeStructure extends Component {
                     <div className="SearchPeople">&lt;Status&gt;
                     <p>Paid</p>
                     </div>
-                    {/* <div className="SearchPeople"><button id="connectdecline" style={{backgroundColor:'green',marginTop:'6%'}}>
-                        Pay Now</button></div> */}
                     <div className="SearchPeople"> <button style={{ color: "white", backgroundColor: '#25a0f7' }} type="submit" className="btn">View Receipt</button></div>
                 </div>
-
                 <br></br>
-
                 <div id="year1">
                     <div className="SearchPeople">&lt;Year&gt; </div>
                     <div className="SearchPeople">&lt;College_Name&gt;</div>
@@ -191,43 +175,27 @@ export default class FeeStructure extends Component {
                     <div className="SearchPeople">&lt;Status&gt;
                     <p>Paid</p>
                     </div>
-                    {/* <div className="SearchPeople"><button id="connectdecline" style={{backgroundColor:'green',marginTop:'6%'}}>
-                        Pay Now</button></div> */}
                     <div className="SearchPeople"> <button style={{ color: "white", backgroundColor: '#25a0f7' }} type="submit" className="btn">View Receipt</button></div>
                 </div>
-
                 <br></br>
-
                 <div id="year1">
                     <div className="SearchPeople">&lt;Year&gt; </div>
                     <div className="SearchPeople">&lt;College_Name&gt;</div>
                     <div className="SearchPeople">&lt;Amount&gt;</div>
-                    {/* <div className="SearchPeople">&lt;Status&gt;
-                    <p>Due</p>
-                    </div> */}
                     <div className="SearchPeople"><button id="connectdecline" style={{backgroundColor:'green',marginTop:'6%'}}>
                         Pay Now</button></div>
                     <div className="SearchPeople"> <button style={{ color: "white", backgroundColor: '#25a0f7' }} type="submit" className="btn">View Receipt</button></div>
                 </div>
-
                 <br></br>
-
                 <div id="year1">
                     <div className="SearchPeople">&lt;Year&gt; </div>
                     <div className="SearchPeople">&lt;College_Name&gt;</div>
                     <div className="SearchPeople">&lt;Amount&gt;</div>
-                    {/* <div className="SearchPeople">&lt;Status&gt;
-                    <p>Due</p>
-                    </div> */}
                     <div className="SearchPeople"><button id="connectdecline" style={{backgroundColor:'green',marginTop:'6%'}}>
                         Pay Now</button></div>
                     <div className="SearchPeople"> <button style={{ color: "white", backgroundColor: '#25a0f7' }} type="submit" className="btn">View Receipt</button></div>
                 </div>
-
                 <br></br>
-
-              
-                
             </div>
         )
     }
