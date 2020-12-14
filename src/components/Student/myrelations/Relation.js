72476b51e53862e8ac56f8bc4d502c3f3450e338
Anubhav Saha <anubhavvsaha2001@gmail.com>
2020-12-14 15:23:46 +0530

    Fixed Student Relation Page

--- src/components/Student/myrelations/Relation.js
+++ src/components/Student/myrelations/Relation.js
@@ -1,132 +1,104 @@
-import React, { Component } from 'react'
-import './Relation.css'
-import {Row, Col } from 'reactstrap';
+import React from "react";
+import { Row, Col, ButtonToggle } from "reactstrap";
+import Table from "react-bootstrap/Table";
+import "./Relation.css";
 
-class Relation extends Component {
-    constructor(props) {
-        super(props)
+const Relation = (props) => {
+  let list = [];
+  for (let i = 0; i < 3; i++) {
+    list.push(
+      <div id="Your_relation1">
+        <Table hover striped style={{ backgroundColor: "whitesmoke" }}>
+          <tr>
+            <td className="SearchPeople">First_Name+Last_Name</td>
+            <td className="SearchPeople">College_Name</td>
+            <td className="SearchPeople">Batch</td>
+            <td className="SearchPeople">Discipline</td>
+            <td className="SearchPeople">
+              {" "}
+              <ButtonToggle color="danger">Block</ButtonToggle>{" "}
+            </td>
+            <td>
+              {" "}
+              <ButtonToggle color="primary">Message</ButtonToggle>{" "}
+            </td>
+          </tr>
+        </Table>
+      </div>
+    );
+  }
 
-        this.state = {
-            firstname: "",
-            lastname: "",
-            college: "",
-            batch: "",
-            discipline: "",
-            request: true
-        }
-        // this.college = this.college.bind(this);
-        // this.batch = this.batch.bind(this);
-        // this.discipline = this.discipline.bind(this);
-
-        this.handleButton = this.handleButton.bind(this);
-    }
-    handlefirstname = (event) => {
-        this.setState({ firstname: event.target.value })
-    }
-    handlelastname = (event) => {
-        this.setState({ lastname: event.target.value })
-    }
-    handlecollege(event) {
-        this.setState({ college: event.target.value });
-    }
-    handlebatch(event) {
-        this.setState({ batch: event.target.value });
-    }
-    handlediscipline(event) {
-        this.setState({ discipline: event.target.value });
-    }
-    handlesubmit = (event) => {
-        event.preventDefault();
-    }
-    handleButton() {
-        this.setState(state => ({
-            request: !state.request
-        }));
-    }
-
-    render() {
-        return (
-            <div className="bodyrelation">
-                <div className="Relationbox">
-                        <form onSubmit={this.handlesubmit}>
-                        <Row>
-
-                            <Col sm={4}>
-                                <label>First Name: </label>
-                                <input type="text" value={this.state.firstname} onChange={this.handlefirstname} />
-                            </Col>
-
-                            <Col sm={4}>
-                                    <label>Last Name: </label>
-                                    <input type="text" value={this.state.lastname}
-                                    onChange={this.handlelastname} />
-                            </Col>
-                            <Col sm={4}>
-                                    <label>college: </label>
-                                    <input type="text" value={this.state.college}
-                                    onChange={this.handlelastname} /><br />
-                            </Col>
-                        </Row>
-                        <Row>
-
-                            <Col sm={6}>
-                                <label>batch: </label>
-
-                                <select value={this.state.batch} onChange={this.handlebatch} style={{ width:'40%', margin: "8px 20px" }}>
-                                    <option value={this.state.batch}>Select Batch</option>
-                                </select>
-                                
-                            </Col>
-                            <Col sm={6}>
-                                
-                                    <label>descipline: </label>
-                                
-                                <select value={this.state.discipline} onChange={this.handlediscipline} style={{ width: '40%', margin: "8px 20px" }} >
-                                    <option value={this.state.discipline}>Select Discipline </option>
-                                </select>
-                                
-                            </Col>
-                        </Row>
-                        <button style={{ color: "white", backgroundColor: '#138808' }} type="submit" value="Search" className="btn">Submit</button>
-                            
-                    </form>
-                    <br/>
-                </div>
-                <br />
-                <br />
-
-                <center>
-                    <h1 style={{ fontFamily: 'Montserrat', fontWeight: '700' }}>Your Relation</h1>
-                </center>
-                    <div id="Your_relation1">
-                    <div className="SearchPeople">&lt;First_Name+Last_Name &gt; </div>
-                    <div className="SearchPeople">&lt;College_Name&gt;</div>
-                    <div className="SearchPeople">&lt;Batch_Year&gt;</div>
-                    <div className="SearchPeople">&lt;Discipline&gt;</div>
-                    <div className="SearchPeople"><button onClick={this.handleButton}  id="connectdecline" style={{backgroundColor:'red'}}>
-                        Block User</button></div>
-                    <div className="SearchPeople"> <button style={{ color: "white", backgroundColor: '#25a0f7' }} type="submit" className="btn">Message</button></div>
-                </div>
-                <div id="Your_relation1">
-                    <div className="SearchPeople">&lt;First_Name+Last_Name &gt; </div>
-                    <div className="SearchPeople">&lt;College_Name&gt;</div>
-                    <div className="SearchPeople">&lt;Batch_Year&gt;</div>
-                    <div className="SearchPeople">&lt;Discipline&gt;</div>
-                    <div className="SearchPeople"><button onClick={this.handleButton}  id="connectdecline" style={{backgroundColor:'red'}}>
-                        Block User</button></div>
-                    <div className="SearchPeople"> <button style={{ color: "white", backgroundColor: '#25a0f7' }} type="submit" className="btn">Message</button></div>
-                </div>
-                <div id="Your_relation1">
-                    <div className="SearchPeople">&lt;First_Name+Last_Name &gt; </div>
-                    <div className="SearchPeople">&lt;College_Name&gt;</div>
-                    <div className="SearchPeople">&lt;Batch_Year&gt;</div>
-                    <div className="SearchPeople">&lt;Discipline&gt;</div>
-                    <div className="SearchPeople"><button onClick={this.handleButton}  id="connectdecline" style={{backgroundColor:'red'}}>
-                        Block User</button></div>
-                    <div className="SearchPeople"> <button style={{ color: "white", backgroundColor: '#25a0f7' }} type="submit" className="btn">Message</button></div>
-                </div>
+  return (
+    <div className="RelationFbody">
+      <div className="relationFbox">
+        <Row>
+          <Col sm={6}>
+            <div className="relationInputs">
+              <label>First Name: </label>
+              <input type="text" id="Firstname" placeholder="" />
             </div>
-        )
-    }
-}
+          </Col>
+
+          <Col sm={6}>
+            <div className="relationInputs">
+              <label>Last Name: </label>
+              <input type="text" id="Lastname" placeholder="" />
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col sm={6}>
+            <div className="relationInputs">
+              <label>College: </label>
+              <input type="text" id="College" placeholder="" />
+            </div>
+          </Col>
+          <Col sm={6}>
+            <div className="relationInputs">
+              {" "}
+              <label>Batch: </label>
+              <select>
+                <option>Select Batch</option>
+                <option>2000</option>
+                <option>2001</option>
+              </select>
+            </div>
+          </Col>
+        </Row>
+        <Row>
+        <Col sm={6}>
+        <div className="relationInputs">
+          {" "}
+          <label>Discipline: </label>
+          <select>
+            <option>Select discipline</option>
+            <option>MBA</option>
+            <option>B.Tech</option>
+          </select>
+        </div>
+      </Col>
+        </Row>
+        <button
+          style={{ color: "white", backgroundColor: "#138808" }}
+          type="submit"
+          value="Search"
+          className="btn"
+        >
+          Submit
+        </button>
+        <br />
+      </div>
+      <br />
+      <br />
+      <center>
+        <h1 style={{ fontFamily: "Montserrat", fontWeight: "700" }}>
+          Your Relation
+        </h1>
+      </center>
+      <br />
+      <div style={{ overflow: "auto" }}>{list}</div>
+    </div>
+  );
+};
+
 export default Relation;
