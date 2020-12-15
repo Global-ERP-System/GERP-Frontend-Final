8db2bcde939774957190a409b9fe8f316f66aaf9
Sourabh Kothari <sourabhkothari2014@gmail.com>
2020-12-15 15:59:01 +0530

    Time Table fixed

--- src/components/Student/Fee Structure/FeeStructure.js
+++ src/components/Student/Fee Structure/FeeStructure.js
@@ -50,122 +50,112 @@ export default class FeeStructure extends Component {
     render() {
         return (
             <div className='cont'>
-                <h1 className="Heading" style={{ textAlign: "center", fontFamily: 'Montserrat', fontWeight: '700'}}>Fee Structure</h1>
+                <h1 className="Heading" style={{ textAlign: "center", fontFamily: 'Montserrat', fontWeight: '700' }}>Fee Structure</h1>
                 <hr style={{ width: '80%' }}></hr>
                 <form onSubmit={this.handlesubmit} style={{ marginTop: '0px' }}>
-                        <Row>
-                            <Col sm={5}>
-                                <label style={{ fontFamily: 'Pt sans narrow ', fontWeight: '700' }}>Name:</label>
-                            </Col>
-                            <Col sm={7}>
-                                <input className="form-control" value={this.state.name} onChange={this.handlename} required placeholder="Name" style={{ fontFamily: 'Montserrat Regular', fontWeight: '400' }}/>
-                            </Col>
-                        </Row>
-                        <br></br>
-                        <Row>
-                            <Col sm={5}>
-                                <label style={{ fontFamily: 'Pt sans narrow ', fontWeight: '700' }}>Enrollment No.:</label>
-                            </Col>
-                            <Col sm={7}>
-                                <input className="form-control" value={this.state.enroll} onChange={this.handleenroll} required placeholder="Enrollment No." style={{ fontFamily: 'Montserrat Regular', fontWeight: '400' }}/>
-                            </Col>
-                            </Row>
-                        <br></br>
-                        <Row>
-                            <Col sm={5}>
-                                <label style={{ fontFamily: 'Pt sans narrow ', fontWeight: '700' }}>Semester:</label>
-                            </Col>
-                            <Col sm={7}>
-                                <select title="Semester" className="form-control" name="Semester" placeholder="Select semester" style={{ fontFamily: 'Montserrat Regular', fontWeight: '400' }}>
-                                    <option >Select semester</option>
-                                    <option eventKey="1">I</option>
-                                    <option eventKey="2">II</option>
-                                </select>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col sm={5}>
-                                <label style={{ fontFamily: 'Pt sans narrow ', fontWeight: '700' }}>Sunject:</label>
-                            </Col>
-                            <Col sm={7}>
-                            <select title="Subject Details" className="form-control" name="Subject Details" placeholder="Select subject" style={{ fontFamily: 'Montserrat Regular', fontWeight: '400' }}>
-                                    <option eventKey="1">Subject details</option>
-                                    <option eventKey="2">Subject details</option>
-                                </select>
-                            </Col>
-                        </Row>
-                           <br></br>
-                        <Row>
-                            <Col sm={5}>
-                                <label style={{ fontFamily: 'Pt sans narrow ', fontWeight: '700' }}>Course:</label>
-                            </Col>
-                            <Col sm={7}>
-                                <input className="form-control" value={this.state.course}
-                                    onChange={this.handlecourse} required placeholder="Course" style={{ fontFamily: 'Montserrat Regular', fontWeight: '400' }}/>
-                            </Col>
-                        </Row>
-                        <br></br>
-                        <div >                   
-                            <Row>
-                                <Col sm={5}>
-                                    <p style={{ fontFamily: 'Pt sans narrow ', fontWeight: '700' }}> Previous Fee Status:&nbsp;&nbsp;&nbsp;</p>
-                                </Col>
-                                <Col sm={7}>
-                                    <input className="form-control" value={this.state.Previousfeestatus}
-                                        onChange={this.handlePreviousfeestatus} required style={{ fontFamily: 'Montserrat Regular', fontWeight: '400' }}/>
-                                </Col>
-                            </Row>
-                            <br></br>
-                            <Row>
-                                <Col sm={5}>
-                                    <label style={{ fontFamily: 'Pt sans narrow ', fontWeight: '700' }}> Due Fee Status:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
-                                </Col>
-                                <Col sm={7}>
-                                    <input className="form-control" value={this.state.duefeestatus}
-                                        onChange={this.handleduefeestatus} required style={{ fontFamily: 'Montserrat Regular', fontWeight: '400' }}/>
-                                </Col>
-                            </Row>
-                            <br></br>
-                            <Row>
-                                <Col sm={5}>
-                                    <label style={{ fontFamily: 'Pt sans narrow ', fontWeight: '700' }}> Due Fee Amount:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
-                                </Col>
-                                <Col sm={7}>
-                                    <input className="form-control" value={this.state.duefeeamount}
-                                        onChange={this.handleduefeeamount} required style={{ fontFamily: 'Montserrat Regular', fontWeight: '400' }}/>
-                                </Col>
-                            </Row>
-                            <br></br>
-                            <Row>
-                                <Col sm={5}>
-                                    <label style={{ fontFamily: 'Pt sans narrow ', fontWeight: '700' }}>Last Date for Payment:</label>
-                                </Col>
-                                <Col sm={7}>
-                                    <input className="form-control" value={this.state.lastdateforpayment}
-                                        onChange={this.handlelastdateforpayment} required style={{ fontFamily: 'Montserrat Regular', fontWeight: '400' }}/>
-                                </Col>
-                            </Row>
-                            <br></br>
-                        </div>   
-                        <div className="Display_Button">
-                            <DropdownButton as={ButtonGroup} title="Payment Mode" name="See Details" variant="primary" className="mx-3">
-                                <Dropdown.Item eventKey="1">Subject Details</Dropdown.Item>
-                                <Dropdown.Item eventKey="2">Subject Details</Dropdown.Item>
-                            </DropdownButton>
-                            
-                        <button id="Pay" type="submit" className="btn">Pay</button>
-                        </div>
-                    </form>
+                    <Row>
+                        <Col sm={5}>
+                            <label style={{ fontFamily: 'Pt sans narrow ', fontWeight: '700' }}>Name:</label>
+                        </Col>
+                        <Col sm={7}>
+                            <input className="form-control" value={this.state.name} onChange={this.handlename} required placeholder="Name" style={{ fontFamily: 'Montserrat Regular', fontWeight: '400' }} />
+                        </Col>
+                    </Row>
                     <br></br>
-                <div id="year1">
-                    <div className="SearchPeople">&lt;Year&gt; </div>
-                    <div className="SearchPeople">&lt;College_Name&gt;</div>
-                    <div className="SearchPeople">&lt;Amount&gt;</div>
-                    <div className="SearchPeople">&lt;Status&gt;
-                    <p>Paid</p>
+                    <Row>
+                        <Col sm={5}>
+                            <label style={{ fontFamily: 'Pt sans narrow ', fontWeight: '700' }}>Enrollment No.:</label>
+                        </Col>
+                        <Col sm={7}>
+                            <input className="form-control" value={this.state.enroll} onChange={this.handleenroll} required placeholder="Enrollment No." style={{ fontFamily: 'Montserrat Regular', fontWeight: '400' }} />
+                        </Col>
+                    </Row>
+                    <br></br>
+                    <Row>
+                        <Col sm={5}>
+                            <label style={{ fontFamily: 'Pt sans narrow ', fontWeight: '700' }}>Semester:</label>
+                        </Col>
+                        <Col sm={7}>
+                            <select title="Semester" className="form-control" name="Semester" placeholder="Select semester" style={{ fontFamily: 'Montserrat Regular', fontWeight: '400' }}>
+                                <option >Select semester</option>
+                                <option eventKey="1">I</option>
+                                <option eventKey="2">II</option>
+                            </select>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col sm={5}>
+                            <label style={{ fontFamily: 'Pt sans narrow ', fontWeight: '700' }}>Sunject:</label>
+                        </Col>
+                        <Col sm={7}>
+                            <select title="Subject Details" className="form-control" name="Subject Details" placeholder="Select subject" style={{ fontFamily: 'Montserrat Regular', fontWeight: '400' }}>
+                                <option eventKey="1">Subject details</option>
+                                <option eventKey="2">Subject details</option>
+                            </select>
+                        </Col>
+                    </Row>
+                    <br></br>
+                    <Row>
+                        <Col sm={5}>
+                            <label style={{ fontFamily: 'Pt sans narrow ', fontWeight: '700' }}>Course:</label>
+                        </Col>
+                        <Col sm={7}>
+                            <input className="form-control" value={this.state.course}
+                                onChange={this.handlecourse} required placeholder="Course" style={{ fontFamily: 'Montserrat Regular', fontWeight: '400' }} />
+                        </Col>
+                    </Row>
+                    <br></br>
+                    <div >
+                        <Row>
+                            <Col sm={5}>
+                                <p style={{ fontFamily: 'Pt sans narrow ', fontWeight: '700' }}> Previous Fee Status:&nbsp;&nbsp;&nbsp;</p>
+                            </Col>
+                            <Col sm={7}>
+                                <input className="form-control" value={this.state.Previousfeestatus}
+                                    onChange={this.handlePreviousfeestatus} required style={{ fontFamily: 'Montserrat Regular', fontWeight: '400' }} />
+                            </Col>
+                        </Row>
+                        <br></br>
+                        <Row>
+                            <Col sm={5}>
+                                <label style={{ fontFamily: 'Pt sans narrow ', fontWeight: '700' }}> Due Fee Status:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
+                            </Col>
+                            <Col sm={7}>
+                                <input className="form-control" value={this.state.duefeestatus}
+                                    onChange={this.handleduefeestatus} required style={{ fontFamily: 'Montserrat Regular', fontWeight: '400' }} />
+                            </Col>
+                        </Row>
+                        <br></br>
+                        <Row>
+                            <Col sm={5}>
+                                <label style={{ fontFamily: 'Pt sans narrow ', fontWeight: '700' }}> Due Fee Amount:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
+                            </Col>
+                            <Col sm={7}>
+                                <input className="form-control" value={this.state.duefeeamount}
+                                    onChange={this.handleduefeeamount} required style={{ fontFamily: 'Montserrat Regular', fontWeight: '400' }} />
+                            </Col>
+                        </Row>
+                        <br></br>
+                        <Row>
+                            <Col sm={5}>
+                                <label style={{ fontFamily: 'Pt sans narrow ', fontWeight: '700' }}>Last Date for Payment:</label>
+                            </Col>
+                            <Col sm={7}>
+                                <input className="form-control" value={this.state.lastdateforpayment}
+                                    onChange={this.handlelastdateforpayment} required style={{ fontFamily: 'Montserrat Regular', fontWeight: '400' }} />
+                            </Col>
+                        </Row>
+                        <br></br>
                     </div>
-                    <div className="SearchPeople"> <button style={{ color: "white", backgroundColor: '#25a0f7' }} type="submit" className="btn">View Receipt</button></div>
-                </div>
+                    <div className="Display_Button">
+                        <DropdownButton id="dropdown-item-button" title="Payment Mode" style={{ width: '150px' }} >
+                            <Dropdown.Item as="button">Subject Details</Dropdown.Item>
+                            <Dropdown.Item as="button">Subject Details</Dropdown.Item>
+                        </DropdownButton>
+
+                        <button id="Pay" type="submit" className="btn">Pay</button>
+                    </div>
+                </form>
                 <br></br>
                 <div id="year1">
                     <div className="SearchPeople">&lt;Year&gt; </div>
@@ -181,7 +171,17 @@ export default class FeeStructure extends Component {
                     <div className="SearchPeople">&lt;Year&gt; </div>
                     <div className="SearchPeople">&lt;College_Name&gt;</div>
                     <div className="SearchPeople">&lt;Amount&gt;</div>
-                    <div className="SearchPeople"><button id="connectdecline" style={{backgroundColor:'green',marginTop:'6%'}}>
+                    <div className="SearchPeople">&lt;Status&gt;
+                    <p>Paid</p>
+                    </div>
+                    <div className="SearchPeople"> <button style={{ color: "white", backgroundColor: '#25a0f7' }} type="submit" className="btn">View Receipt</button></div>
+                </div>
+                <br></br>
+                <div id="year1">
+                    <div className="SearchPeople">&lt;Year&gt; </div>
+                    <div className="SearchPeople">&lt;College_Name&gt;</div>
+                    <div className="SearchPeople">&lt;Amount&gt;</div>
+                    <div className="SearchPeople"><button id="connectdecline" style={{ backgroundColor: 'green', marginTop: '6%' }}>
                         Pay Now</button></div>
                     <div className="SearchPeople"> <button style={{ color: "white", backgroundColor: '#25a0f7' }} type="submit" className="btn">View Receipt</button></div>
                 </div>
@@ -190,7 +190,7 @@ export default class FeeStructure extends Component {
                     <div className="SearchPeople">&lt;Year&gt; </div>
                     <div className="SearchPeople">&lt;College_Name&gt;</div>
                     <div className="SearchPeople">&lt;Amount&gt;</div>
-                    <div className="SearchPeople"><button id="connectdecline" style={{backgroundColor:'green',marginTop:'6%'}}>
+                    <div className="SearchPeople"><button id="connectdecline" style={{ backgroundColor: 'green', marginTop: '6%' }}>
                         Pay Now</button></div>
                     <div className="SearchPeople"> <button style={{ color: "white", backgroundColor: '#25a0f7' }} type="submit" className="btn">View Receipt</button></div>
                 </div>
