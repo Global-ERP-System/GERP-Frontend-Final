40493ff124d4e4bc66dfb3ea0e750033de51843b
Anubhav Saha <anubhavvsaha2001@gmail.com>
2020-12-17 16:10:00 +0530

    Fixed Exam Page

--- src/components/Student/Exam/ExaminationBacklog.js
+++ src/components/Student/Exam/ExaminationBacklog.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
-import ExaminationGrading from './ExaminationGrading'
-import ExaminatioUpcoming from './ExaminatioUpcoming'
-import './Examination.css'
+import Table from 'react-bootstrap/Table';
+import './Examination.css';
 
 export default class Examination extends Component {
     constructor(props) {
@@ -19,35 +18,131 @@ export default class Examination extends Component {
 
     render() {
         return (
-            <div>
-                <div className="Main_Backlog">
-                    <h1 style={{ fontFamily: 'Montserrat', fontWeight: '700', textAlign: "center" }}>Examination</h1>
-                    <br></br>
-                    <div className="Mainbox_Backlog">
-                        <div className="Header_Backlog">
-                            <h2 style={{ textAlign: "center" }}>BackLogs</h2>
-                        </div>
-                        <div className="valueFeild_Backlog">
-                            <select value={this.state.visibility} onChange={this.handleVisibilityChange} id="Visibility2">
-                                <option value="Public">1st Sem </option>
-                                <option value="ClassMate">2nd Sem</option>
-                            </select>
-                            <h3>Subject </h3>
-                            <h3>Backlogs </h3>
-                        </div>
-                        <div className="Values_Backlog">
-                            <h3>1 </h3>
-                            <h3>DBMS </h3>
-                            <div className="alignment">
-                                <h3>No </h3>
-                            </div>
-
-                        </div>
-
-                    </div>
-                    <ExaminationGrading></ExaminationGrading>
-                    <ExaminatioUpcoming></ExaminatioUpcoming>
+            <div className='container-fluid'>
+                <h1 style={{ fontFamily: 'Montserrat', fontWeight: '700', textAlign: "center" }}>Examination</h1>
+                <div className='table-container'>
+                    <Table striped bordered hover>
+                        <thead>
+                        <tr>
+                        <th colSpan='3'>Backlogs</th>
+                        </tr>
+                        <tr>
+                            <th>
+                                <select>
+                                    <option>Semester</option>
+                                    <option>1</option>
+                                    <option>2</option>
+                                </select>
+                            </th>
+                            <th>Subject</th>
+                            <th>Backlogs</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>DBMS</td>
+                            <td>No</td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>DBMS</td>
+                            <td>No</td>
+                        </tr>
+                        <tr>
+                            <td>3</td>
+                            <td>DBMS</td>
+                            <td>No</td>
+                        </tr>
+                        <tr>
+                            <td>4</td>
+                            <td>DBMS</td>
+                            <td>No</td>
+                        </tr>
+                        </tbody>
+                    </Table>
+                    <Table striped bordered hover>
+                        <thead>
+                        <tr>
+                        <th colSpan='3'>Grading</th>
+                        </tr>
+                        <tr>
+                            <th>
+                                <select>
+                                    <option>Semester</option>
+                                    <option>1</option>
+                                    <option>2</option>
+                                </select>
+                            </th>
+                            <th>Subject</th>
+                            <th>Backlogs</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>DBMS</td>
+                            <td>No</td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>DBMS</td>
+                            <td>No</td>
+                        </tr>
+                        <tr>
+                            <td>3</td>
+                            <td>DBMS</td>
+                            <td>No</td>
+                        </tr>
+                        <tr>
+                            <td>4</td>
+                            <td>DBMS</td>
+                            <td>No</td>
+                        </tr>
+                        </tbody>
+                    </Table>
+                    <Table striped bordered hover>
+                        <thead>
+                        <tr>
+                        <th colSpan='3'>Upcoming Exam</th>
+                        </tr>
+                        <tr>
+                            <th>
+                                <select>
+                                    <option>Semester</option>
+                                    <option>1</option>
+                                    <option>2</option>
+                                </select>
+                            </th>
+                            <th>Subject</th>
+                            <th>Backlogs</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>DBMS</td>
+                            <td>No</td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>DBMS</td>
+                            <td>No</td>
+                        </tr>
+                        <tr>
+                            <td>3</td>
+                            <td>DBMS</td>
+                            <td>No</td>
+                        </tr>
+                        <tr>
+                            <td>4</td>
+                            <td>DBMS</td>
+                            <td>No</td>
+                        </tr>
+                        </tbody>
+                    </Table>
                 </div>
+                
             </div>
         )
     }
